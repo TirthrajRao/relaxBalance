72cdcba7f427610f4304f680ff3dade476f40233
bhavik103 <bhavikkalariya103@gmail.com>
2020-07-04 18:21:12 +0530

    french app done

---
+++ src/app/alert-popover/alert-popover.component.ts
@@ -0,0 +1,148 @@
+import { Component, OnInit } from '@angular/core';
+import { InAppPurchase2, IAPProduct } from '@ionic-native/in-app-purchase-2/ngx';
+import { Platform, NavParams } from '@ionic/angular';
+import { ToastController, PopoverController, AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-alert-popover',
+  templateUrl: './alert-popover.component.html',
+  styleUrls: ['./alert-popover.component.scss'],
+})
+export class AlertPopoverComponent implements OnInit {
+  product: IAPProduct;
+  productId = [{
+    id: 'psychowalkmanfr1',
+    type: this.iap2.PAID_SUBSCRIPTION
+  }, {
+    id: 'psychowalkmanfr2',
+    type: this.iap2.PAID_SUBSCRIPTION
+  }, {
+    id: 'psychowalkmanfr3',
+    type: this.iap2.PAID_SUBSCRIPTION
+  }
+  ]
+  modalType: any;
+  text: any;
+  constructor(
+    public toastController: ToastController,
+    public alertController: AlertController,
+    public popover: PopoverController,
+    public platform: Platform,
+    private iap2: InAppPurchase2,
+    private navParams: NavParams
+
+  ) { }
+
+  ngOnInit() {
+    this.platform.ready().then(async () => {
+      this.setup();
+    });
+    this.modalType = this.navParams.data.type;
+    console.log(this.modalType);
+  }
+
+  setup() {
+    this.iap2.register(this.productId);
+    this.iap2.refresh();
+  }
+
+  registerHandlersForPurchase(productId) {
+    let self = this.iap2;
+    this.iap2.when(productId).updated(function (product) {
+      if (product.loaded && product.valid && product.state === self.APPROVED && product.transaction != null) {
+        product.finish();
+      }
+    });
+    this.iap2.when(productId).registered((product: IAPProduct) => {
+      // alert(` owned ${product.owned}`);
+    });
+    this.iap2.when(productId).owned((product: IAPProduct) => {
+      // alert(` owned ${product.owned}`);
+      product.finish();
+    });
+    this.iap2.when(productId).approved((product: IAPProduct) => {
+      // alert('approved');
+      product.finish();
+    });
+    this.iap2.when(productId).refunded((product: IAPProduct) => {
+      // alert('refunded');
+    });
+    this.iap2.when(productId).expired((product: IAPProduct) => {
+      // alert('expired');
+    });
+  }
+
+  checkout(productId) {
+    // this.registerHandlersForPurchase(productId);
+    try {
+      let product = this.iap2.get(productId);
+      console.log('Product Info: ', product);
+      this.iap2.order(productId).then((p) => {
+        this.iap2.when(productId).owned((product: IAPProduct) => {
+          console.log(` owned ${product.owned}`);
+          console.log(p);
+          product.finish();
+        });
+        // alert('Purchase Succesful' + JSON.stringify(p));
+      }).catch((e) => {
+        alert('Error Ordering From Store' + e);
+      });
+    } catch (err) {
+      console.log('Error Ordering ' + JSON.stringify(err));
+    }
+  }
+
+  async cancel() {
+    const toast = await this.toastController.create({
+      message: "Votre période d'essai est terminée!",
+      duration: 2000
+    });
+    toast.present();
+    this.popover.dismiss();
+  }
+
+  dismiss(trialInfo) {
+    this.popover.dismiss(trialInfo);
+  }
+
+  restoreExistingSubsription(){
+    
+  }
+
+  async discountedCheckout(productId) {
+    const alert = await this.alertController.create({
+      inputs: [
+        {
+          name: 'discountCode',
+          type: 'password',
+          placeholder: 'Veuillez saisir le mot de passe ici  '
+        }],
+      buttons: [
+        {
+          text: 'Annuler',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: "je suis d'accord",
+          handler: async (alertData) => {
+            if(alertData.discountCode == 'BALANCE'){
+              this.checkout(productId);
+            }else{
+              const toast = await this.toastController.create({
+                message: 'Veuillez saisir le mot de passe correct',
+                position: 'bottom',
+                duration: 3000
+              });
+              toast.present();
+            }     
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+}
+
---
+++ src/app/inner-page/inner-page.page.ts
@@ -0,0 +1,133 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Platform, IonRange } from '@ionic/angular';
+import { Howl, Howler } from 'howler';
+import { LoadingController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-inner-page',
+  templateUrl: './inner-page.page.html',
+  styleUrls: ['./inner-page.page.scss'],
+})
+export class InnerPagePage implements OnInit {
+  musicPath: any;
+  type: any;
+  sound: any;
+  playlist = [];
+  activeTrack = null;
+  player: any;
+  isPlaying = false;
+  totalTimePlayed: any = "00:00"
+  @ViewChild('range', { static: false }) range: IonRange;
+  progress = 0;
+  duration: any = "00:00";
+  constructor(
+    public loadingController: LoadingController,
+    private route: ActivatedRoute,
+    public platform: Platform,
+  ) {
+    this.checkForType();
+  }
+
+  ionViewWillLeave() {
+    if (this.type == 'music') {
+      this.player.stop();
+    }
+  }
+  ionViewWillEnter() {
+
+
+  }
+  ngOnInit() {
+  }
+
+  //check for type if music/text
+  checkForType() {
+    this.type = this.route.snapshot.params.type;
+
+    if (this.type == 'music') {
+      this.musicPath = this.route.snapshot.params.name
+      this.musicPath = './assets/music/' + this.musicPath;
+      this.start();
+    }
+  }
+
+  //creating audio object and playing audio file 
+  async start() {
+    const loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'Loading audio. Please wait...'
+    });
+    await loading.present();
+
+    if (this.player) {
+      this.player.stop();
+    }
+    this.player = new Howl({
+      src: [this.musicPath],
+      html5: true,
+      onplay: async () => {
+        await loading.dismiss();
+        this.isPlaying = true;
+        this.duration = await this.display(this.player.duration());
+        this.activeTrack = this.musicPath;
+        this.updateProgress();
+      },
+      onend: () => {
+        this.player.stop();
+        this.isPlaying = false;
+      }
+    });
+
+    this.player.play();
+  }
+
+  //function for play & pause 
+  togglePlayer(pause) {
+    this.isPlaying = !pause;
+    if (pause) {
+      this.player.pause();
+    } else {
+      this.player.play();
+    }
+  }
+
+  //for adding some seconds to current value
+  seek(value) {
+    let newValue = +value;
+    let duration = this.player.duration();
+    this.player.seek(duration * (newValue / 100));
+  }
+
+  //updating progress bar for audio file
+  async updateProgress() {
+    let seek = this.player.seek();
+    this.totalTimePlayed = await this.display(Math.floor(seek));
+    this.progress = (seek / this.player.duration()) * 100 || 0;
+    setTimeout(() => {
+      this.updateProgress();
+    }, 1000);
+  }
+
+  //moving backward or forward
+  move(type, progress) {
+    let newValue = +progress;
+    let duration = this.player.duration();
+    if (type == 'forward') {
+      let currentTime = duration * (newValue / 100);
+      this.player.seek(currentTime + 15);
+    } else if (type == 'backward') {
+      let currentTime = duration * (newValue / 100);
+      this.player.seek(currentTime - 15);
+    }
+  }
+
+  //making displayable format for audio timer
+  display(seconds) {
+    const format = val => `0${Math.floor(val)}`.slice(-2)
+    // const hours = seconds / 3600
+    const minutes = (seconds % 3600) / 60
+
+    return [minutes, seconds % 60].map(format).join(':')
+  }
+}
